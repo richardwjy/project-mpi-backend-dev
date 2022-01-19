cb56a41ad427ee49c98213498b3a8ff145190622
Richard <richard_huang0309@yahoo.com>
2022-01-19 11:16:15 +0700

    Cors Policy add exposedHeaders

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const PORT = process.env.PORT || 5000
 
 dotenv.config()
 
+const corsOptions = {
+    allowedHeaders: ['Authorization']
+}
+
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 
 // app.get('/', (req, res) => {
 //     res.send('Welcome to original page')
